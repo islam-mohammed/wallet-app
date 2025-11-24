cb93dcffaaee5c6ad6072465412cc388a39d49f5
Islam Elbastaweesy <islam.elbastaweesy@gmail.com>
2025-11-25 00:17:19 +0200

    feat: set up file-based routing with unplugin-vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
 import './style.css'
+import router from './router'
 import App from './App.vue'
 
-createRouter({
-  history: createWebHistory(),
-  routes,
-})
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(router)
+app.mount('#app')
